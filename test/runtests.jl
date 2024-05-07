b04ce9f3cdeaaa2a90ce62239329ca3c97d474fc
mtfishman <mfishman@flatironinstitute.org>
2024-05-07 08:10:19 -0400

    Skip tests if there is no CUDA installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,27 +4,31 @@ end
 
 using ITensorGPU, Test, CUDA
 
-println("Running ITensorGPU tests with a runtime CUDA version: $(CUDA.runtime_version())")
+if !CUDA.has_cuda()
+  println("System does not have CUDA, skipping test.")
+else
+  println("Running ITensorGPU tests with a runtime CUDA version: $(CUDA.runtime_version())")
 
-CUDA.allowscalar(false)
-@testset "ITensorGPU.jl" begin
-  #@testset "$filename" for filename in ("test_cucontract.jl",)
-  #  println("Running $filename with autotune")
-  #  cmd = `$(Base.julia_cmd()) -e 'using Pkg; Pkg.activate(".."); Pkg.instantiate(); include("test_cucontract.jl")'`
-  #  run(pipeline(setenv(cmd, "CUTENSOR_AUTOTUNE" => 1); stdout=stdout, stderr=stderr))
-  #end
-  @testset "$filename" for filename in (
-    "test_dmrg.jl",
-    "test_cuitensor.jl",
-    "test_cudiag.jl",
-    "test_cudense.jl",
-    "test_cucontract.jl",
-    "test_cumpo.jl",
-    "test_cumps.jl",
-    "test_cutruncate.jl",
-    #"test_pastaq.jl",
-  )
-    println("Running $filename")
-    include(filename)
+  CUDA.allowscalar(false)
+  @testset "ITensorGPU.jl" begin
+    #@testset "$filename" for filename in ("test_cucontract.jl",)
+    #  println("Running $filename with autotune")
+    #  cmd = `$(Base.julia_cmd()) -e 'using Pkg; Pkg.activate(".."); Pkg.instantiate(); include("test_cucontract.jl")'`
+    #  run(pipeline(setenv(cmd, "CUTENSOR_AUTOTUNE" => 1); stdout=stdout, stderr=stderr))
+    #end
+    @testset "$filename" for filename in (
+      "test_dmrg.jl",
+      "test_cuitensor.jl",
+      "test_cudiag.jl",
+      "test_cudense.jl",
+      "test_cucontract.jl",
+      "test_cumpo.jl",
+      "test_cumps.jl",
+      "test_cutruncate.jl",
+      #"test_pastaq.jl",
+    )
+      println("Running $filename")
+      include(filename)
+    end
   end
 end

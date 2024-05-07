05d91f3d1f965fd11670127dfebde242cf2ebb03
Documenter.jl <documenter@juliadocs.github.io>
2024-05-07 12:27:04 +0000

    build based on cf88d62

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorGPU","category":"page"},{"location":"#ITensorGPU","page":"Home","title":"ITensorGPU","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorGPU.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorGPU]","category":"page"}]
+}
